581ef92e64b347a5b45d64854e37aee31b2c8f6b
Nathan Hannon <nathan@hannon.me>
2021-05-19 16:43:30 -0400

    Added firebase support

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebase: {
+    apiKey: "AIzaSyBQO0gKpuofXqfvZbEAU5mjhwWGFw6AOdU",
+    authDomain: "kanbanfire-f584c.firebaseapp.com",
+    projectId: "kanbanfire-f584c",
+    storageBucket: "kanbanfire-f584c.appspot.com",
+    messagingSenderId: "894013926910",
+    appId: "1:894013926910:web:c4e75fe8342c3c838024af",
+    measurementId: "G-V7S1JY2351"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebase: {
+    apiKey: "AIzaSyBQO0gKpuofXqfvZbEAU5mjhwWGFw6AOdU",
+    authDomain: "kanbanfire-f584c.firebaseapp.com",
+    projectId: "kanbanfire-f584c",
+    storageBucket: "kanbanfire-f584c.appspot.com",
+    messagingSenderId: "894013926910",
+    appId: "1:894013926910:web:c4e75fe8342c3c838024af",
+    measurementId: "G-V7S1JY2351"
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
